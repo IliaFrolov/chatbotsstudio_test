8deb6f1bc24adb6838719b192811c5e1c3112f97
Ilia <ilia.frolov@simedis.com>
2020-07-01 00:32:01 +0300

    feat: add lesson controller and router

---
+++ src/controllers/lessons.controller.js
@@ -0,0 +1,106 @@
+const db = require('../models');
+
+const Lesson = db.lesson;
+
+exports.create = (req, res) => {
+  // console.log(req.body);
+  const value = req.body.topic;
+  if (!value) {
+    res.status(400).send({ message: 'Content can not be empty!' });
+    return;
+  }
+
+  const lesson = new Lesson({
+    topic: req.body.topic,
+    teachers: req.body.teachers,
+    group: req.body.group,
+    classRoom: req.body.classRoom,
+    time: req.body.time,
+  });
+  lesson
+    .save(lesson)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while creating the lesson.',
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  Lesson.find()
+    .populate('teachers')
+    .populate({ path: 'group', populate: { path: 'students' } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500)
+        .send({ message: err.message || 'Some error occurred while retrieving Lesson.' });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const { id } = req.params;
+
+  Lesson.findById(id)
+    .populate('teachers')
+    .populate({ path: 'group', populate: { path: 'students' } })
+    .then((data) => {
+      if (!data) { res.status(404).send({ message: `Not found Lesson with id ${id}` }); } else res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: err.message || `Error retrieving Lesson with id=${id}` });
+    });
+};
+
+exports.update = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Data to update can not be empty!',
+    });
+  }
+  const { id } = req.params;
+
+  Lesson.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Lesson with id=${id}. Maybe Lesson was not found!`,
+        });
+      } else res.send({ message: 'Lesson was updated successfully.' });
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(500).send({
+        message: err.message || `Error updating Lesson with id=${id}`,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const { id } = req.params;
+
+  Lesson.findByIdAndRemove(id, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Lesson with id=${id}. Maybe Lesson was not found!`,
+        });
+      } else {
+        res.send({
+          message: 'Lesson was deleted successfully!',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Could not delete Lesson with id=${id}`,
+      });
+    });
+};
---
+++ src/routes/lessons.routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const lessons = require('../controllers/lessons.controller.js');
+
+module.exports = (app) => {
+  router.post('/', lessons.create);
+
+  router.get('/', lessons.findAll);
+
+  router.get('/:id', lessons.findOne);
+
+  router.put('/:id', lessons.update);
+
+  router.delete('/:id', lessons.delete);
+
+  app.use('/api/lessons', router);
+};
